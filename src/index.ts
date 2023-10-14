1c995951414c46c01230a0fecbb0432ae92f7cec
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2023-10-14 19:22:14 +0530

    mongoose create op done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import taskRoute from "./task"
 import cors from "cors";
-import mongoose from "mongoose"
+import mongoose,{Schema,model,connect} from "mongoose"
 import dotenv from "dotenv"
+import * as uuid from "uuid"
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,21 +18,48 @@ app.use("/tasks",taskRoute)
 const run = async () =>{
     try {
         // @ts-ignore
-        await  mongoose.connect(process.env.MONGOURL);
+        await  mongoose.connect(process.env.MONGOVRENT,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to Remote DB")
+        console.log(uuid.v4())
     } catch (e) {
         console.log(e)
     }
 
 }
-
+// Connect to DB
 run();
 
+// User model
+interface IUser {
+    cname:string;
+    age:number;
+    profile?:string;
+}
+
+const userSchema = new Schema<IUser>({
+    cname:{type:String},
+    age:{type:Number},
+    profile:String
+})
+
+const User = model<IUser>("User",userSchema);
 
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.jsonp({message: 'Hello, TypeScript Express!'});
+//
+
+
+app.get('/', async (req: Request, res: Response) => {
+    const user = new User({
+        cname:"J Dalal Mahapatra",
+        age:27,
+        profile:`${uuid.v4()}`
+    })
+    const _user = await user.save();
+    res.jsonp({message: 'Hello, TypeScript Express!',user:_user});
 });
 
 app.get('/health',(req:Request, res:Response)=>{
