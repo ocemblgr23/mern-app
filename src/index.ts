33d5c3bc470bb0c5839c7f3f129e19776a037720
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-18 15:20:18 +0530

    CRUD Operation done for worker API

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import express, { Request, Response } from 'express';
-import taskRoute from "./task"
-import cors from "cors";
-import mongoose,{Schema,model,connect} from "mongoose"
-import dotenv from "dotenv"
-import * as uuid from "uuid"
-import workerRoute from "./routes/worker"
-import courseRoute from "./routes/course"
-import {run } from "./config/connection"
+import taskRoute from './task';
+import cors from 'cors';
+import mongoose, { Schema, model, connect } from 'mongoose';
+import dotenv from 'dotenv';
+import * as uuid from 'uuid';
+import workerRoute from './routes/worker';
+import courseRoute from './routes/course';
+import { run } from './config/connection';
 import { errorHandeler } from './config/error-middleware';
 import multer from 'multer';
+import path from 'path';
+import sharp from 'sharp';
+import * as fs from 'fs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 dotenv.config();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/uploads",express.static("uploads"))
+app.use('/uploads', express.static('uploads'));
 
 //Todo add tasks route
-app.use("/tasks",taskRoute)
-
+app.use('/tasks', taskRoute);
 
 // const run = async () =>{
 //     try {
@@ -43,58 +44,73 @@ run();
 
 // User model start here
 interface IUser {
-    cname:string;
-    age:number;
-    profile?:string;
+  cname: string;
+  age: number;
+  profile?: string;
 }
 // Create schema
 const userSchema = new Schema<IUser>({
-    cname:{type:String},
-    age:{type:Number},
-    profile:String
-})
+  cname: { type: String },
+  age: { type: Number },
+  profile: String,
+});
 // Create a model
-const User = model<IUser>("User",userSchema);
+const User = model<IUser>('User', userSchema);
 
 // Todo add worker route
-app.use("/worker",workerRoute);
-app.use("/apiv1/course",courseRoute);
+app.use('/worker', workerRoute);
+app.use('/apiv1/course', courseRoute);
 
 app.get('/', async (req: Request, res: Response) => {
-    const user = new User({
-        cname:"J Dalal Mahapatra",
-        age:27,
-        profile:`${uuid.v4()}`
-    })
-    const _user = await user.save();
-    res.jsonp({message: 'Hello, TypeScript Express!',user:_user});
+  const user = new User({
+    cname: 'J Dalal Mahapatra',
+    age: 27,
+    profile: `${uuid.v4()}`,
+  });
+  const _user = await user.save();
+  res.jsonp({ message: 'Hello, TypeScript Express!', user: _user });
 });
 
-app.get('/health',(req:Request, res:Response)=>{
-    res.status(200).jsonp({
-        message:"Health Check done."
-    })
-})
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).jsonp({
+    message: 'Health Check done.',
+  });
+});
+
+const UPLOAD_PATH = path.join(path.resolve(), '/uploads');
 
 // File uplaod
 const storage = multer.diskStorage({
-    destination:"./uploads",
-    filename:(req,file,cb)=>{
-        cb(null,Date.now()+"_"+file.originalname)
-    }
-})
-
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    cb(null, uuid.v4() + '_' + file.originalname);
+  },
+});
 // const upload = multer({dest:"uploads/"})
-const upload = multer({storage:storage})
-app.post("/upload",upload.single("image"),(req:Request,res:Response)=>{
+const upload = multer({ storage: storage });
+app.post(
+  '/upload',
+  upload.single('image'),
+  async (req: Request, res: Response) => {
+    fs.access('./uploads', (error) => {
+      if (error) {
+        fs.mkdirSync('./uploads');
+      }
+    });
 
-res.status(201).jsonp({file:req.file?.filename})
-})
-
+    // const timestamp = new Date().toISOString();
+    // const ref = `${timestamp}-${req.file?.originalname}.webp`;
+    // await sharp(req.file?.filename).webp({ quality: 20 }).toFile('./uploads'+ref);
+    // console.log();
+    res.status(201).jsonp({ file: req.file?.filename });
+    // compress the image and save
+    // const compress = await sharp(image.path).resize().jpeg({quality:50}).toFile(image.path);
+    // console.log(compress);
+  }
+);
 
 app.use(errorHandeler);
 
-app.listen(port,()=>{
-    console.log(`Server running under http://localhost:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Server running under http://localhost:${port}`);
+});
--- src/routes/worker.ts
+++ src/routes/worker.ts
@@ -1,6 +1,7 @@
 import { NextFunction } from 'express';
 import { Router, Request, Response } from 'express';
 import { IWorker, Worker } from '../models/worker-model';
+import * as HTTP_STATUS_CODES from "http-status"
 
 const router = Router();
 // Add your CRUD API implementation here
@@ -26,7 +27,7 @@ router.post('/', async (req: Request, res: Response,next:NextFunction) => {
   try {
     //  Create into DB
     const newWorkerProfile = await newWorker.save();
-    res.status(201).jsonp({ newWorkerProfile });
+    res.status(HTTP_STATUS_CODES.CREATED).jsonp({ newWorkerProfile });
   } catch (error) {
     next(error)
     // console.log('Error in saving worker to the database');
@@ -35,7 +36,58 @@ router.post('/', async (req: Request, res: Response,next:NextFunction) => {
 
 router.get('/', async (req: Request, res: Response) => {
   const workers = await Worker.find({});
-  res.status(200).jsonp(workers);
+  res.status(HTTP_STATUS_CODES.OK).jsonp(workers);
+});
+
+router.get('/:id', async (req: Request, res: Response,next:NextFunction) => {
+  let id = req.params.id;
+  try {
+    const worker = await Worker.findById({_id:id});
+    if(!worker) {
+      res.status(HTTP_STATUS_CODES.NOT_FOUND).jsonp({
+        message:`${id} : Didn't Match Any Worker.`
+      })
+      return;
+    }
+    res.status(HTTP_STATUS_CODES.OK).jsonp(worker);
+  } catch (error) {
+    next(error)
+  }
+
+});
+
+router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+  try {
+    const worker = await Worker.findById({_id:id});
+    if(!worker) {
+      res.status(HTTP_STATUS_CODES.NOT_FOUND).jsonp({
+        message:`${id} : Didn't Match Any Worker.`
+      })
+      return;
+    }
+    const updatedWorker = await Worker.updateOne(req.body);
+    res.status(HTTP_STATUS_CODES.OK).jsonp(updatedWorker);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+  try {
+    const worker = await Worker.findById({_id:id});
+    if(!worker) {
+      res.status(HTTP_STATUS_CODES.NOT_FOUND).jsonp({
+        message:`${id} : Didn't Match Any Worker.`
+      })
+      return;
+    }
+    const deleteWorker = await Worker.deleteOne({_id:id});
+    res.status(HTTP_STATUS_CODES.OK).jsonp(deleteWorker);
+  } catch (error) {
+    next(error)
+  }
 });
 
 export default router;
